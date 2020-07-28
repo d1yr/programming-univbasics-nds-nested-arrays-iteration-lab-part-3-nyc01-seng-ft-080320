21b41ac677660c0be6b3f1f273fe4c0c7c5572f4
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-28 16:11:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ while row_index < src.count do
   row_index += 1
 end
 results = results.grep(String)
-results.join
+results.join(" ")
 end
  # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
