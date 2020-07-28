d406e40207f5ce4534f943188b836f8b6bc57dc3
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-28 15:31:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ results = []
 src.each{|array|
 array.each{|k|
 results << src[array][k]
+}
 end
  # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
