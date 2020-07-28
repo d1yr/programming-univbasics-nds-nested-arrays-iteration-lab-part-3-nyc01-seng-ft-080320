b1a2e61e9b85aa5ffe1a48c0f3ba61e184edf6bf
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-28 16:03:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,12 +5,11 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count 
-  if src[row_index][element_index] !=0
+  
     results << src[row_index][element_index]
     element_index += 1
-  else
-    src[row_index][element_index].pop
-  end
+
+
   end
   row_index += 1
 end
